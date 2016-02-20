19f07b9aa2d6570f44c431e62ad4d9fea29edc61
Mateusz Sienkan <mateusz@sienkan.pl>
2016-02-20 09:00:30 +0100

    Add Add tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,7 +6,26 @@ angular.module('starter.controllers', [])
     });
   })
 
-  .controller('AddCtrl', function ($scope) {
+  .controller('AddCtrl', function ($scope, $cordovaCamera) {
+    $scope.takeImage = function() {
+      var options = {
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 250,
+        targetHeight: 250,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.srcImage = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        // error
+      });
+    }
   })
 
   .controller('OrganizationsCtrl', function ($scope, Organizations) {
